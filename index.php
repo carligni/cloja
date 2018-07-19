764e7f4c7bb5dd2374da73ff83732ae23ca32d37
Carlos Silva <carfsilva@gmail.com>
2018-07-19 19:07:12 +0100

    slim route

--- index.php
+++ index.php
@@ -2,21 +2,19 @@
 
 require_once("vendor/autoload.php");
 
-// $app = new \Slim\Slim();
+use Hcode\Db\Sql;
 
-// $app->config('debug', true);
+$app = new \Slim\Slim();
 
-// $app->get('/', function() {
-//     $sql = new Hcode\DB\Sql();
-//     $results = $sql->select("SELECT * FROM tb_users");
-//     echo json_encode($results);
-//     // echo "OK";
-// });
+$app->config('debug', true);
 
-// $app->run();
+$app->get('/', function() {
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_users");
+    echo json_encode($results);
+    // echo "OK";
+});
 
-$sql = new Hcode\Db\Sql();
-$results = $sql->select("SELECT * FROM tb_users");
-echo json_encode($results);
+$app->run();
 
 ?>
