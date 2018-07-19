e937aaf952c86a46e925da0d6826b55c538006e7
Carlos Silva <carfsilva@gmail.com>
2018-07-19 18:56:21 +0100

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+// $app = new \Slim\Slim();
+
+// $app->config('debug', true);
+
+// $app->get('/', function() {
+//     $sql = new Hcode\DB\Sql();
+//     $results = $sql->select("SELECT * FROM tb_users");
+//     echo json_encode($results);
+//     // echo "OK";
+// });
+
+// $app->run();
+
+$sql = new Hcode\Db\Sql();
+$results = $sql->select("SELECT * FROM tb_users");
+echo json_encode($results);
+
+?>
